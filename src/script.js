b82f392dceac4cdfd610a2a0adecf2bce447f309
Moween <moween1998@gmail.com>
2021-05-01 21:15:19 +0100

    add code

---
+++ src/script.js
@@ -0,0 +1,53 @@
+const main = document.getElementById('container');
+const showTimeContainer = document.createElement('h1');
+main.append(showTimeContainer);
+
+
+
+function Clock({ template }) {
+  let timer;
+  
+  function render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+
+    let mins = date.getMinutes();
+    if (mins < 10) {
+      mins = '0' + mins;
+    }
+
+    let secs = date.getSeconds();
+    if (secs < 10) {
+      secs = '0' + secs;
+    }
+
+    let output = template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+      
+      showTimeContainer.textContent = output;
+  }
+
+  this.stop = function() {
+    clearInterval(timer);
+  };
+
+  this.start = function() {
+    render();
+    timer = setInterval(render, 1000);
+  };
+
+}
+
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
+
+  
+
+
